c0ba4bee5d92a203cbbf1c55bb376b6f0240a796
neurons <niranjanvp@gmail.com>
2014-08-18 11:59:54 -0400

    All tests passing

---
+++ variation/src/main/java/org/dishevelled/variation/adam/Contig.java
@@ -0,0 +1,86 @@
+package org.dishevelled.variation.adam;
+
+/*
+
+    dsh-variation  Variation.
+    Copyright (c) 2013-2014 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+
+public class Contig
+{
+    private String name;
+    private long  contigLength;
+    private String reference;
+    private String species;
+
+    public Contig()
+    {
+
+    }
+
+    public Contig(String name, long contigLength, String reference, String species)
+    {
+        this.name = name;
+        this.contigLength = contigLength;
+        this.reference = reference;
+        this.species = species;
+    }
+
+    public String getContigName()
+    {
+        return name;
+    }
+
+    public long getContigLength()
+    {
+        return contigLength;
+    }
+
+    public String getAssembly()
+    {
+        return reference;
+    }
+
+    public String getSpecies()
+    {
+        return species;
+    }
+
+    public void setContigLength(long contigLength)
+    {
+        this.contigLength = contigLength;
+    }
+
+    public void setContigName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setAssembly(String reference)
+    {
+        this.reference = reference;
+    }
+
+    public void setSpecies(String species)
+    {
+        this.species = species;
+    }
+}
---
+++ variation/src/main/java/org/dishevelled/variation/adam/Variant.java
@@ -0,0 +1,98 @@
+package org.dishevelled.variation.adam;
+
+/*
+
+    dsh-variation  Variation.
+    Copyright (c) 2013-2014 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+
+public class Variant
+{
+    private Contig contig;
+    private long position;
+    private long exclusiveEnd;
+    private String referenceAllele;
+    private String variantAllele;
+
+    public Variant()
+    {
+
+    }
+
+    public Variant(Contig contig, long position, long exclusiveEnd, String referenceAllele, String variantAllele)
+    {
+        this.contig = contig;
+        this.position = position;
+        this.exclusiveEnd = exclusiveEnd;
+        this.referenceAllele = referenceAllele;
+        this.variantAllele = variantAllele;
+    }
+
+    public Contig getContig()
+    {
+        return contig;
+    }
+
+    public long getPosition()
+    {
+        return position;
+    }
+
+    public long getExclusiveEnd()
+    {
+        return exclusiveEnd;
+    }
+
+    public String getReferenceAllele()
+    {
+        return referenceAllele;
+    }
+
+    public String getVariantAllele()
+    {
+        return variantAllele;
+    }
+
+    public void setContig(Contig contig)
+    {
+        this.contig = contig;
+    }
+
+    public void setPosition(long position)
+    {
+        this.position = position;
+    }
+
+    public void setExclusiveEnd(long exclusiveEnd)
+    {
+        this.exclusiveEnd = exclusiveEnd;
+    }
+
+    public void setReferenceAllele(String referenceAllele)
+    {
+        this.referenceAllele = referenceAllele;
+    }
+
+    public void setVariantAllele(String variantAllele)
+    {
+        this.variantAllele = variantAllele;
+    }
+}
